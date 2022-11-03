221905471d730c37e8ea9c8a007818304e955a3d
m511452334 <m511452334@qq.com>
2022-11-03 21:37:18 +0800

    第一次提交

---
+++ life-helper/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -0,0 +1,43 @@
+package cn.itcast.order.web;
+
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.service.OrderService;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @SentinelResource(value = "hot")
+    @GetMapping("{orderId}")
+    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+        // 根据id查询订单并返回
+        return orderService.queryOrderById(orderId);
+    }
+
+    @GetMapping("/query")
+    public String queryOrder() {
+        orderService.queryGoods();
+        return "查询订单成功";
+    }
+
+    @GetMapping("/save")
+    public String saveOrder() {
+        orderService.queryGoods();
+        return "保存订单陈宫";
+    }
+
+    @GetMapping("/update")
+    public String updateOrder() {
+        return "更新订单成功";
+    }
+}
